5a382862c7820290488fb86514b3fc30cbcddf37
TheEpiCreator <53312465+TheEpiCreator@users.noreply.github.com>
2021-01-09 19:41:58 -0600

    Changed angle added HTMLKit

--- js/backgroundPrototype.js
+++ js/backgroundPrototype.js
@@ -1,34 +1,50 @@
 // Import 3D lib
-import * as THREE from "../node_modules/three/build/three.module.js";
+import * as THREE from "../node_modules/three/build/three.module.js"
+import * as HTMLKit from "./lib/HTMLKit.js"
 const SimplexNoise = require('simplex-noise')
 
 // setup scene
 var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 5
+camera.position.y = -4
+camera.rotation.x = 0.2
 // setup render pipeline
 var renderer = new THREE.WebGLRenderer({ antialias: true })
 // setup canvas
-const canvas = renderer.domElement
+// Create HTMLTag instance
+var rendererElement = new HTMLKit.Tag("p")
+rendererElement.reference(renderer.domElement)
+rendererElement.toPosition("last")
+rendererElement.tag.classList.add("bg")
 
-renderer.setSize(window.innerWidth, window.innerHeight)
+
+const resize = () => {
+    const width = window.innerWidth - 10
+    const height = window.innerHeight - 10
+    renderer.setSize(width, height)
+    camera.fov = 30
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+}
+resize()
 document.body.appendChild(renderer.domElement)
 
 // setup lighting
 var light = new THREE.AmbientLight(0x404040, 0.2)
 scene.add(light)
 
-var dirLight = new THREE.DirectionalLight(0xf0f0f0, .1)
+var dirLight = new THREE.DirectionalLight(0xffffff, .1)
 dirLight.position.set(0, 0, 1)
 scene.add(dirLight)
 
 // setup object to animate
 var geometry = new THREE.PlaneGeometry(10, 10, 100, 100)
 
-var material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF, vertexColors: THREE.FaceColors, side: THREE.SingleSide, shininess: 100 })
+var material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF, vertexColors: THREE.FaceColors, side: THREE.DoubleSide, shininess: 100 })
 
-var flag = new THREE.Mesh(geometry, material)
-scene.add(flag)
+var BgObj = new THREE.Mesh(geometry, material)
+scene.add(BgObj)
 
 // Animate mesh
 const clock = new THREE.Clock()
@@ -46,20 +62,17 @@ function animate() {
 
     const t = clock.getElapsedTime()
 
-    flag.geometry.vertices.map(v => {
-        const waveX1 = 0.5 * Math.sin(v.x * 2 + t)
-        const waveX2 = 0.25 * Math.sin(v.x * 3 + t)
-        const waveY1 = 0.1 * Math.sin(v.y + t)
-        const multi = (v.x + 2.5) / 5
-
-        //v.z = (waveX1 + waveX2 + waveY1) * multi;
-        v.z = (simplex.noise2D(v.x * 4 + t * .25, v.y * 4 + t * .25) / 5)
+    BgObj.geometry.vertices.map(v => {
+        v.z = (simplex.noise2D(v.x * 3 + t * .25, v.y * 3 + t * .25) / 5)
     })
 
-    geometry.colorsNeedUpdate = true
-    flag.geometry.verticesNeedUpdate = true
+    geometry.colorsNeedUpdate = false
+    BgObj.geometry.verticesNeedUpdate = true
 
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
 }
 animate()
+
+// Dynamically resize canvas
+window.addEventListener("resize", resize)
